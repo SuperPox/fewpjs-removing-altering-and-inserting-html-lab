044834cbf4a918b79f9203ade6837c126e5c0148
SuperPox <calebkohl@gmail.com>
2021-04-13 19:51:14 -0600

    commit tree 95f7da204c8320a5f7c9b392520d792725214c97

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+document.querySelector("main#main").remove()
+
+const newHeader = document.createElement("H1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Bob is the champion!";
+newHeader.className = "victory";
+
+element.style.backgroundColor = '#27647B';
+element.style.textAlign = 'center';
+ul.style.textAlign = 'left'
